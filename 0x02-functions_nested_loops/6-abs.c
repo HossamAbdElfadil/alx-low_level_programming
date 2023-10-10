ce03b2a489efcae273326d61c981d46b1e61c1fe
Hossam Abdelfadil <7ossam.abdelfadil@gmail.com>
2023-10-10 14:28:08 -0700

    a

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _abs - Computers the absolute
  * @c: The number computed
@@ -10,7 +11,8 @@ int _abs(int c)
 	if (c < 0)
 	{
 		int abs_val;
-		
+
+
 		abs_val = c * -1;
 		return (abs_val);
 	}
